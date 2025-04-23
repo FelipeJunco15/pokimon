ad7584494ed1d60e7f46441bbb315f22d499fba5
FelipeJunco15 <juncofelipe15@gmail.com>
2025-04-22 23:30:58 -0500

    Add files via upload

---
+++ POKIMOON - copia/js/Entrenador.js
@@ -0,0 +1,16 @@
+
+
+
+function GetDataUser(){
+    let dataUser = localStorage.getItem("user")
+    
+    if(dataUser == null){
+        alert('No has iniciado sesion')
+        window.location.href = "../login.html"
+    }
+    
+}
+
+GetDataUser()
+
+
